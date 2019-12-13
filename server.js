d8d6a75841f5ff58fb4a6bbc0a449d28465428b9
John Gorter <john.gorter@gmail.com>
2019-12-13 23:19:32 +0100

    clientside

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 let express = require('express');
 require('dotenv').config();
 let app = express();
-let data = require('./data.js');
 let loadContent = require('./apis/mongodb');
-let fetchTaskers = require('./apis/taskers');
+// let fetchTaskers = require('./apis/taskers');
 
 app.use(express.static('.'));
 
@@ -11,15 +10,12 @@ app.use(express.static('.'));
 app.set('view engine', 'pug');
 
 app.get('*', async (req, res) => {
-  console.log(req.params[0]);
     if (req.params[0].indexOf('.') > 0) {
-      console.log("responding with file");
       res.sendFile(req.params[0]);
     } else {
         let key = `www.taskbooker.be${req.params[0]}`
         const content = await loadContent(key)
-        const taskers = await renderTaskers(req.params[0])
-        console.log('server: ', taskers);
+      //  const taskers = await renderTaskers(req.params[0])
       if (content)
         res.render('index', { ...content[0] });
       else
@@ -34,12 +30,12 @@ app.listen(3131, () => {
 });
 
 
-const renderTaskers = async (params) => {
-  let categoriesList = params.split('/')
+// const renderTaskers = async (params) => {
+//   let categoriesList = params.split('/')
 
-  // disregard if last param is location
-  let lastCategory = categoriesList.length === 4 ? categoriesList[categoriesList.length -2] : categoriesList[categoriesList.length -1]
-  const taskers = await fetchTaskers(lastCategory)
-  console.log(taskers);
-  return taskers
-}
+//   // disregard if last param is location
+//   let lastCategory = categoriesList.length === 4 ? categoriesList[categoriesList.length -2] : categoriesList[categoriesList.length -1]
+//   const taskers = await fetchTaskers(lastCategory)
+//   console.log(taskers);
+//   return taskers
+// }

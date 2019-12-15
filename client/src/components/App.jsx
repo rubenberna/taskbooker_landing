144d32003703e11751b4c18cc1e7163b7fc49774
rubenberna <rubenmbernardes@gmail.com>
2019-12-15 18:15:28 +0100

    chore(): client structure

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,42 @@
 let React = require('react');
+let getTaskers = require('../../../server/apis/taskers');
 
-const App = () => {
-  return(
-    <div>
-      Hello
-    </div>
-  )
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      taskers: []
+    };
+  }
+
+  async componentDidMount() {
+    let URLParams = window.location.pathname.split('/')
+
+    // search taskers per last category
+    let categoryToBeSearched = URLParams.length === 4 ? URLParams[URLParams.length -2] : URLParams[URLParams.length -1]
+    const res = await getTaskers(categoryToBeSearched)
+    console.log(res);
+    this.setState({ taskers: res })
+  }
+
+  renderTaskers() {
+    const { taskers } = this.state
+    if(taskers.length) {
+      return taskers.map(t =>
+          (<div key={t.id}>
+            <p><span style={{ fontWeight: 600 }}>id:</span>{t.id}</p>
+          </div>
+        )
+      )
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderTaskers()}
+      </div>
+    );
+  }
 }
-
-export default App;
+module.exports = App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,34 +1,5 @@
 let React = require('react');
-let ReactDOM = require('react-dom');
-let TaskersList = require('./components/Taskerslist');
-
-const q = `
-{
-  listActiveUsers(filter:{categoryIds:["b591a956-10aa-4bfb-bad4-92ddfb8b81a6"]}) {
-    records {
-      nodes {
-        id,
-        avatar {
-          url
-        },
-           prettyName,
-        rating,
-        description,
-        categories {
-            nodes {
-            title
-          }
-        }
-      }
-    }
-  }
-}
-`
-
-fetch('https://api.stg.taskbooker.be/public/graphql', { method:'POST', headers:{'Content-Type':'application/json'}, body:JSON.stringify({ query: q })})
-.then((response) => response.json())
-.then((t) => {
-   console.log(t.data.listActiveUsers.records.nodes);
-    let App = ({taskers}) => <div>{JSON.stringify(taskers)} <TaskersList></TaskersList></div>
-    ReactDOM.render(<App taskers={t} />, document.querySelector("#app"));
-});
+let { hydrate } = require('react-dom');
+let App = require('./components/App');
+setTimeout(() => hydrate(<App />, document.querySelector("#app")) ,1000)
+// timeout because the #app selector is not created until the first call is made from the server

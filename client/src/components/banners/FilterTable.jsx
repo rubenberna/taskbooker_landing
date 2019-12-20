61301b561c6cbfdbc8433eaeaacbc617f72b856f
rubenberna <rubenmbernardes@gmail.com>
2019-12-20 16:07:00 +0100

    style(): added manar's style manually due to conflict in merging the bundle.js

--- client/src/components/banners/FilterTable.jsx
+++ client/src/components/banners/FilterTable.jsx
@@ -1,75 +1,99 @@
-const React = require('react');
-const axios = require('axios');
+const React = require("react");
+const axios = require("axios");
 
-const limitResults = 200
+const limitResults = 200;
 
 class FilterTable extends React.Component {
   constructor() {
     super();
     this.state = {
       tableList: [],
-      selectedCategory: 'hulp-voor-tablet',
-      selectedCity: '',
+      selectedCategory: "",
+      selectedCity: "",
       citiesList: [],
       categoriesList: []
-    }
+    };
   }
 
   async componentDidMount() {
-    const res = await axios.post('/queryRedis', {limitResults})
-    const data  = res.data
+    const res = await axios.post("/queryRedis", { limitResults });
+    const data = res.data;
     this.setState({
       tableList: data.shortlist,
       categoriesList: data.uniqueCategories,
       citiesList: data.uniqueCities
-    })
+    });
   }
 
-  addFilter(name, value) {
-    let change = {}
-    change[name] = value
-    this.setState({ change })
-    this.filterResults()
+  async handleCategory(e) {
+    await this.setState({ selectedCategory: e.target.value });
+    this.filterResults();
+  }
+  async handleCity(e) {
+    await this.setState({ selectedCity: e.target.value });
+    this.filterResults();
   }
 
   async filterResults() {
-    const { selectedCategory, selectedCity } = this.state
-    const res = await axios.post('/queryRedis/filter', {selectedCategory, selectedCity})
-    const data  = res.data
-    this.setState({ tableList: data })
+    const { selectedCategory, selectedCity } = this.state;
+    const res = await axios.post("/queryRedis/filter", {
+      selectedCategory,
+      selectedCity
+    });
+    const data = res.data;
+    this.setState({ tableList: data });
   }
 
   render() {
-    const { tableList } = this.state
-    return(
-      <div className='container'>
-        <button onClick={ this.filterResults.bind(this) }>Filter</button>
-        <table class="table">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">URL</th>
-              <th scope="col">Breadcrumb1</th>
-              <th scope="col">Breadcrumb2</th>
-              <th scope="col">Breadcrumb3</th>
-              <th scope="col">CityPostalcode</th>
-            </tr>
-          </thead>
-          <tbody>
-        {tableList.map((item, i) =>
-          <tr key={item._id}>
-            <td>{i}</td>
-            <td>{item.URL}</td>
-            <td>{item.Breadcrumb1}</td>
-            <td>{item.Breadcrumb2}</td>
-            <td>{item.Breadcrumb3}</td>
-            <td>{item.CityPostalcode}</td>
-          </tr>
-        )}
-        </tbody>
-        </table>
+    const { tableList, citiesList, categoriesList } = this.state;
+    return (
+      <div className="container">
+        <div className="seo-pages-cities-select">
+          <label>
+            <span>Activity</span>
+            <span class="select-wrapper">
+              <select
+                onChange={this.handleCategory.bind(this)}
+                className="select"
+              >
+                {categoriesList.map(category => (
+                  <option key={category} value={category}>
+                    {category}
+                  </option>
+                ))}
+              </select>
+            </span>
+          </label>
+          <label>
+            <span>Province</span>
+            <select onChange={this.handleCity.bind(this)} className="select">
+              <option></option>
+              {citiesList.map(cityCodePostal => (
+                <option key={cityCodePostal} value={cityCodePostal}>
+                  {cityCodePostal}
+                </option>
+              ))}
+            </select>
+          </label>
+        </div>
+
+        <ul className="seo-pages-cities-list">
+          {tableList.map((item, i) => (
+            <li key={item._id} className="seo-pages-cities-item">
+              <a href={item.URL}>
+                {item.Breadcrumb1.replace(/-/g, " ")
+                  .charAt(0)
+                  .toUpperCase() +
+                  item.Breadcrumb1.replace(/-/g, " ").slice(1)}{" "}
+                {item.Breadcrumb2.replace(/-/g, " ")}
+                {item.Breadcrumb3.replace(/-/g, " ")}{" "}
+                {item.CityPostalcode.replace(/-/g, " ")}
+              </a>
+            </li>
+          ))}
+        </ul>
       </div>
-    )
+    );
   }
 }
 

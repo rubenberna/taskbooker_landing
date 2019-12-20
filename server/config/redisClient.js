571e1ef7f561fd81ebb13fe26a496eddf22984cc
rubenberna <rubenmbernardes@gmail.com>
2019-12-20 22:01:45 +0100

    feat(): created redis client based on node_env

--- server/config/redisClient.js
+++ server/config/redisClient.js
@@ -4,10 +4,10 @@ const REDIS_PORT = process.env.PORT || 6379
 
 const client = asyncRedis.createClient(process.env.REDISCLOUD_URL, {no_ready_check: true})
 
-function defineClient() {
-  if(process.env.NODE_ENV !== 'production') return asyncRedis.createClient({port: REDIS_PORT, password: process.env.REDIS_PASSWORD})
-  else return asyncRedis.createClient(process.env.REDISCLOUD_URL, {no_ready_check: true});
-}
+// function defineClient() {
+//   if(process.env.NODE_ENV !== 'production') return asyncRedis.createClient({port: REDIS_PORT, password: process.env.REDIS_PASSWORD})
+//   else return asyncRedis.createClient(process.env.REDISCLOUD_URL, {no_ready_check: true});
+// }
 
 const sendResponse = (key) => {
   console.log(`Set ${key} in redis`);
